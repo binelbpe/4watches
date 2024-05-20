baadf99825a390c5631ee5e79db55cc151490926
Binel Biju <binelbpe@gmail.com>
2024-05-20 18:40:15 +0530

    added google api

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "http://4watches.shop/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
